8c4d9339b05cf181060f256925e487427b11ac0b
Ishika Kashyap <ishikakashyap2412@gmail.com>
2023-07-02 10:01:18 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <b>PORTFOLIO</b>
+    <div>
+      <NavBar/> 
+      <Home/>
+      <About/>
+      <Portfolio/>
+      <SocialLinks/>
     </div>
   );
 }
